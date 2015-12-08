2ed94f3c853c6032325df9211b64a706eb89b866
Caleb Callaway <cqcallaw@gmail.com>
2015-12-07 21:54:15 -0800

    Don't add event until after task completion

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -47,8 +47,8 @@ scheduler.every '10s' do
     if task.instance_of?(ReminderTask)
       Rails.logger.debug('Dequeuing reminder task')
       reminder_task = task.becomes(ReminderTask)
-      reminder = Reminder.create(reporter: task.reporter)
       reminder_task.execute
+      reminder = Reminder.create(reporter: task.reporter)
       task.reporter.events.push(reminder)
       task.reporter.task = nil
       task.destroy
@@ -56,8 +56,8 @@ scheduler.every '10s' do
     elsif task.instance_of?(NotifyTask)
       Rails.logger.debug('Dequeuing notification task')
       notify_task = task.becomes(NotifyTask)
-      reminder = Notification.create(reporter: task.reporter)
       notify_task.execute
+      reminder = Notification.create(reporter: task.reporter)
       task.reporter.events.push(reminder)
       task.reporter.task = nil
       task.destroy
