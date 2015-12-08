055c26e128287f7383b3fc0b4262631bec7568a9
Caleb Callaway <cqcallaw@gmail.com>
2015-12-07 23:01:28 -0800

    More detailed logging

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -7,14 +7,14 @@ scheduler.every '5s' do
   User.all.each do |user|
     user.reporters.all.each do |reporter|
       interval = Time.now - reporter.latest_event.created_at
-      Rails.logger.debug("Reporter name: #{reporter.name}")
-      Rails.logger.debug("Reporter time_interal: #{reporter.time_interval}")
-      Rails.logger.debug("Reporter latest event type: #{reporter.latest_event.class.name}")
-      Rails.logger.debug("Reporter latest_event.created_at: #{reporter.latest_event.created_at}")
-      Rails.logger.debug("Interval: #{interval}")
-      Rails.logger.debug("Reporter task: #{reporter.task}")
+      Rails.logger.debug("[Producer] Reporter name: #{reporter.name}")
+      Rails.logger.debug("[Producer] Reporter time_interal: #{reporter.time_interval}")
+      Rails.logger.debug("[Producer] Reporter latest event type: #{reporter.latest_event.class.name}")
+      Rails.logger.debug("[Producer] Reporter latest_event.created_at: #{reporter.latest_event.created_at}")
+      Rails.logger.debug("[Producer] Interval: #{interval}")
+      Rails.logger.debug("[Producer] Reporter task: #{reporter.task}")
       unless reporter.task.nil?
-        Rails.logger.debug("Reporter task type: #{reporter.task.class.name}")
+        Rails.logger.debug("[Producer] Reporter task type: #{reporter.task.class.name}")
       end
 
       unless reporter.latest_event.instance_of?(Invite) || reporter.latest_event.instance_of?(Notification) || reporter.latest_event.instance_of?(Optout)
@@ -23,18 +23,18 @@ scheduler.every '5s' do
           if reporter.latest_event.instance_of?(Reminder)
             # push notification task onto task queue
             # only push one task per reporter
-            Rails.logger.debug("Enqueuing notification task for #{reporter.name} of user #{user.id}")
+            Rails.logger.debug("[Producer] Enqueuing notification task for #{reporter.name} of user #{user.id}")
             reporter.task = NotifyTask.create
-            Rails.logger.debug('Finished enqueuing of notification task')
+            Rails.logger.debug('[Producer] Finished enqueuing of notification task')
           elsif interval > reporter.time_interval && reporter.task.nil?
             # push reminder task onto task queue
             # only push one task per reporter
-            Rails.logger.debug("Enqueuing reminder task for #{reporter.name} of user #{user.id}")
+            Rails.logger.debug("[Producer] Enqueuing reminder task for #{reporter.name} of user #{user.id}")
             reporter.task = ReminderTask.create
-            Rails.logger.debug('Finished enqueuing of reminder task')
+            Rails.logger.debug('[Producer] Finished enqueuing of reminder task')
           end
         else
-          Rails.logger.debug("No action required for reporter #{reporter.name}")
+          Rails.logger.debug("[Producer] No action required for reporter #{reporter.name}")
         end
       end
     end
@@ -45,23 +45,23 @@ scheduler.every '10s' do
   # consume tasks
   Task.all.each do |task|
     if task.instance_of?(ReminderTask)
-      Rails.logger.debug('Dequeuing reminder task')
+      Rails.logger.debug('[Consumer] Dequeuing reminder task')
       reminder_task = task.becomes(ReminderTask)
       reminder_task.execute
       reminder = Reminder.create(reporter: task.reporter)
       task.reporter.events.push(reminder)
       task.reporter.task = nil
       task.destroy
-      Rails.logger.debug('Finished processing reminder task')
+      Rails.logger.debug('[Consumer] Finished processing reminder task')
     elsif task.instance_of?(NotifyTask)
-      Rails.logger.debug('Dequeuing notification task')
+      Rails.logger.debug('[Consumer] Dequeuing notification task')
       notify_task = task.becomes(NotifyTask)
       notify_task.execute
       reminder = Notification.create(reporter: task.reporter)
       task.reporter.events.push(reminder)
       task.reporter.task = nil
       task.destroy
-      Rails.logger.debug('Finished processing notification task')
+      Rails.logger.debug('[Consumer] Finished processing notification task')
     end
   end
 end
