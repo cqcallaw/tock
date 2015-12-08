f94d847e4100ae06d4b846311286fd2736bbde8b
Caleb Callaway <cqcallaw@gmail.com>
2015-12-07 21:50:55 -0800

    Fix host name in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'brainvitamins.net/tock' }
+  config.action_mailer.default_url_options = { host: 'brainvitamins.net' }
   config.action_mailer.smtp_settings = {
     authentication: :plain,
     address: 'smtp.mailgun.org',
